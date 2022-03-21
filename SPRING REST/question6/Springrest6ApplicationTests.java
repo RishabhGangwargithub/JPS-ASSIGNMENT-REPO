52bcc6a635044d630f38d959f895668c685dd236
RishabhGangwargithub <99122825+RishabhGangwargithub@users.noreply.github.com>
2022-03-21 20:19:42 +0530

    Add files via upload

---
+++ SPRING REST/question6/Springrest6ApplicationTests.java
@@ -0,0 +1,13 @@
+package Question6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springrest6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
