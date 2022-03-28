3f700cd0d7df2a2f9caf269ca91105d4a3604bc6
RishabhGangwargithub <99122825+RishabhGangwargithub@users.noreply.github.com>
2022-03-28 21:37:30 +0530

    Add files via upload

---
+++ SPRING SECURITY/ASSINGNMENT-1/Q2/SpringSecurityQ2Application.java
@@ -0,0 +1,13 @@
+package com.springRESTSecurityQ2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityQ2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityQ2Application.class, args);
+	}
+
+}
---
+++ SPRING SECURITY/ASSINGNMENT-1/Q2/restController.java
@@ -0,0 +1,15 @@
+package com.springRESTSecurityQ2;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class restController {
+	
+	@GetMapping("/hello")
+	public String Hello() {
+		String s = "<h1>HelloWorld</h1>" ;
+		return (s);
+		
+	}
+}
