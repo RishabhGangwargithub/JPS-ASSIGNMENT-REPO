b5b4a13fa0336a1f59c5a6bd25b52401c5dc6507
RishabhGangwargithub <99122825+RishabhGangwargithub@users.noreply.github.com>
2022-03-30 13:23:45 +0530

    Add files via upload

---
+++ SPRING SECURITY/ASSIGNMENT-2/question1/SpringSecurityA21ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springSecurityA21;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityA21ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
